cded6331eeb1e7b34406e08975cbd29333d09ac3
Furkan Gunes <furkanguness2003@gmail.com>
2024-07-08 13:28:27 +0300

    dosyalar oluşturuldu ve düzenlendi

--- src/main/java/com/furkangunes/_07_Scanner.java
+++ src/main/java/com/furkangunes/_07_Scanner.java
@@ -6,7 +6,7 @@ public class _07_Scanner {
     public static void main(String[] args) {
         // Kullanıcıda veri almak istersek
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Lütfen adınızı giriniz:");
+        System.out.println("Lütfen adınızı giriniz");
         String name = scanner.nextLine();
         System.out.println("Adınız: " + name);
         scanner.close();
