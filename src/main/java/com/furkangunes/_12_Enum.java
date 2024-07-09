758453c725e7b1ef825bc9483de93be26c225b61
Furkan Gunes <furkanguness2003@gmail.com>
2024-07-09 12:40:38 +0300

    updated

--- src/main/java/com/furkangunes/_12_Enum.java
+++ src/main/java/com/furkangunes/_12_Enum.java
@@ -1,4 +1,18 @@
 package com.furkangunes;
 
+enum ETicaret{
+    SMALL, MEDIUM, LARGE, XLARGE
+}
 public class _12_Enum {
+    public static void main(String[] args) {
+        ETicaret etc = ETicaret.LARGE;
+        System.out.println(etc);
+
+        System.out.println(etc.hashCode());
+        System.out.println(etc.ordinal());
+
+        // String'e çevir
+        String etic = ETicaret.XLARGE.toString();
+        System.out.println(etic.toUpperCase());
+    }
 }
