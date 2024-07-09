d9f5b1a7d10c97d4daf34c5aab2b9217d415340e
Furkan Gunes <furkanguness2003@gmail.com>
2024-07-09 10:39:23 +0300

    updated

---
+++ src/main/java/com/furkangunes/_09_2_StringTokenizer.java
@@ -0,0 +1,15 @@
+package com.furkangunes;
+
+import java.util.StringTokenizer;
+
+public class _09_2_StringTokenizer {
+    public static void main(String[] args) {
+        String value="Java Javase Javaee javame Servlet*JSP~JSF_SpringMVC+SpringData-";
+
+        // Parçalasın
+        StringTokenizer tokenizer = new StringTokenizer(value,"*~_+- ");
+        while (tokenizer.hasMoreTokens()){
+            System.out.println(tokenizer.nextToken());
+        }
+    }
+}
